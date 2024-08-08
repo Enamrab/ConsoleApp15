de537e4f60d6cf43438550156ef95e23d380d2b6
Enam.Rab <Enam.Rab@C-AP-PG03MYJ2>
2024-08-08 23:07:09 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Rectangle
+    {
+        public int x;
+        public int y;
+        Rectangle(int x, int y=0)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        static int  CalArea(int x,int y)
+        {
+            return x * y;
+        }
+        static int CalRectangle(int x, int y)
+        {
+            return x + y;
+        }
+    }
+    internal class Class1
+    {
+    }
+}
